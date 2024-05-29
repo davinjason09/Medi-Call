f81fa7fbd2bd3abae9bd55288728ae242e032f2f
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2024-05-29 18:10:56 +0700

    chore: added packages and added logo & styles

---
+++ constants/Styles.ts
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+import Colors from "./Colors";
+
+export const defaultStyles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  button: {
+    width: "57.5%",
+    height: 45,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 4,
+  },
+  textHeading1: {
+    fontSize: 24,
+    fontFamily: "Inter",
+    fontWeight: "bold",
+  },
+  textHeading2: {
+    fontSize: 20,
+    fontFamily: "Inter",
+    fontWeight: "semibold",
+  },
+  textSubHeading: {
+    fontSize: 16,
+    fontFamily: "Inter",
+    fontWeight: "medium",
+  },
+  textBody: {
+    fontSize: 14,
+    fontFamily: "Inter",
+    fontWeight: "regular",
+  },
+  textLight: {
+    fontSize: 12,
+    fontFamily: "Inter",
+    fontWeight: "light",
+  },
+  textExtraLight: {
+    fontSize: 12,
+    fontFamily: "Inter",
+    fontWeight: "ultralight",
+  }
+})
